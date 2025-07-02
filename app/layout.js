8b8a53228d89b3ca4538a44666566034dea27c77
Marius Daniel Vasilcanu <74616980+MariusVasilcanu28@users.noreply.github.com>
2025-07-02 15:35:42 +0300

    wip

--- app/layout.js
+++ app/layout.js
@@ -5,10 +5,11 @@ const josefin = Josefin_Sans({
   display: "swap",
 });
 
+import { Analytics } from "@vercel/analytics/next";
 import { ReservationProvider } from "@/app/_components/ReservationContext";
 import "@/app/_styles/globals.css";
-import Header from "./_components/Header";
 import { Toaster } from "react-hot-toast";
+import Header from "./_components/Header";
 
 export const metadata = {
   title: {
@@ -30,11 +31,13 @@ export default function RootLayout({ children }) {
 
         <div className="flex align-middle justify-center flex-1 px-8 py-12">
           <main className="max-w-7xl mx-auto w-full">
-            <ReservationProvider>{children}</ReservationProvider>
+            <ReservationProvider>
+              {children}
+              <Analytics />
+              <Toaster />
+            </ReservationProvider>
           </main>
         </div>
-
-        <Toaster />
       </body>
     </html>
   );
